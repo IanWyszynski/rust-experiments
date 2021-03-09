03cecbee59525194c20cce20b175b889472c0598
Jan Wyszynski <jm.wyszynski@gmail.com>
2021-03-09 01:18:29 -0500

    logger + astar

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rust Experiments
+
+## Wiki Explorer
+
+A program to find a chain of wikipedia article links between pages for two search terms. These
+are output as graphviz .dot descriptions.
+
+![[Boeing AH-64 Apache](https://en.wikipedia.org/wiki/Boeing_AH-64_Apache) to [Shattered Sword](https://en.wikipedia.org/wiki/Shattered_Sword)](./examples/wiki.png)
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,11 +9,69 @@ use rayon::prelude::*;
 use std::fmt::*;
 use std::fs;
 
-const BREADTH: i32 = 1;
-const DEPTH: i32 = 1;
+use log::LevelFilter;
+use log::*;
+use log4rs::append::console::ConsoleAppender;
+use log4rs::append::file::FileAppender;
+use log4rs::config::{Appender, Config, Logger, Root};
+
+const BREADTH: i32 = 5;
+const DEPTH: i32 = 4;
+
+const DEBUG_TARGET: &str = "debug";
+const GRAPH_TARGET: &str = "graph";
+
+fn setup_logger() {
+    let stdout = ConsoleAppender::builder()
+        .target(log4rs::append::console::Target::Stdout)
+        .build();
+
+    let graph = FileAppender::builder()
+        .encoder(Box::new(log4rs::encode::pattern::PatternEncoder::new(
+            "{m}",
+        )))
+        .build("graph.dot")
+        .unwrap();
+
+    let stderr = ConsoleAppender::builder()
+        .target(log4rs::append::console::Target::Stderr)
+        .build();
+
+    log4rs::init_config(
+        Config::builder()
+            .appender(Appender::builder().build("graph", Box::new(graph)))
+            .appender(Appender::builder().build("stdout", Box::new(stdout)))
+            .appender(Appender::builder().build("stderr", Box::new(stderr)))
+            .logger(
+                Logger::builder()
+                    .appender("graph")
+                    .additive(false)
+                    .build(GRAPH_TARGET, LevelFilter::Info),
+            )
+            .logger(
+                Logger::builder()
+                    .appender("stderr")
+                    .additive(false)
+                    .build(DEBUG_TARGET, LevelFilter::Info),
+            )
+            .build(Root::builder().appender("stdout").build(LevelFilter::Info))
+            .unwrap(),
+    )
+    .unwrap();
+}
+
+fn timed<T>(label: &str, f: &mut FnMut() -> T) -> T {
+    let now = std::time::Instant::now();
+    let result = f();
+    info!("{} took {}", label, now.elapsed().as_micros());
+    result
+}
 
 fn main() {
-    let mut graph: petgraph::graph::Graph<String, String> = petgraph::graph::Graph::new();
+    setup_logger();
+
+    let mut graph: petgraph::graph::UnGraph<String, String> =
+        petgraph::graph::UnGraph::new_undirected();
 
     let mut words: Vec<String> = fs::read_to_string("/usr/share/dict/words")
         .expect("Error reading dictionary")
@@ -26,12 +84,27 @@ fn main() {
     let wiki = wikipedia::Wikipedia::<wikipedia::http::default::Client>::default();
     let wiki_ref = &wiki;
 
-    let updates = search_wiki(wiki_ref, &search_terms)
+    let results = timed("search_wikis", &mut || search_wiki(wiki_ref, &search_terms));
+
+    let terms = results.iter().map(|n| &n.1).collect::<Vec<_>>();
+
+    info!("Crawling articles: '{:?}'", terms);
+
+    let updates = results
         .par_iter()
         .map(|term_to_title| {
+            debug!(
+                target: DEBUG_TARGET,
+                "Spawned thread {:?}",
+                std::thread::current().id()
+            );
             let mut results = vec![];
-            crawl_from_title(wiki_ref, &term_to_title.1, &mut results, BREADTH, 0);
-            println!(
+            let ref_results = &mut results;
+            timed(&format!("crawl title: {}", &term_to_title.1), &mut || {
+                crawl_from_title(wiki_ref, &term_to_title.1, ref_results, BREADTH, 0)
+            });
+            debug!(
+                target: DEBUG_TARGET,
                 "Received {} results crawling '{}'",
                 results.len(),
                 term_to_title.1
@@ -41,18 +114,37 @@ fn main() {
         .flatten()
         .collect::<Vec<_>>();
 
-    println!("Generated {} updates.", updates.len());
+    debug!(target: DEBUG_TARGET, "Generated {} updates.", updates.len());
 
-    let (nodes, edges) = update_graph(&updates, &mut graph);
-    println!("{} nodes, {} edges", nodes, edges);
+    let g_ref = &mut graph;
+    let (nodes, edges) = timed("update_graph", &mut || update_graph(&updates, g_ref));
+    debug!(target: DEBUG_TARGET, "{} nodes, {} edges", nodes, edges);
 
-    println!(
+    let endpoints = results
+        .iter()
+        .map(|n| &n.1)
+        .map(|n| g_ref.node_indices().find(|ni| &g_ref[*ni] == n).unwrap())
+        .collect::<Vec<_>>();
+
+    debug!(
+        target: DEBUG_TARGET,
         "Generated graph with {} nodes and {} edges",
         graph.node_count(),
         graph.edge_count()
     );
 
-    println!("{}", Dot::new(&graph));
+    timed("astar", &mut || match petgraph::algo::astar(
+        &graph,
+        endpoints[0],
+        |n| n == endpoints[1],
+        |_| 1,
+        |_| 0,
+    ) {
+        Some((_, path)) => info!("{:?}", path),
+        None => info!("No path found between endpoints: {:?}", endpoints),
+    });
+
+    info!(target: GRAPH_TARGET, "{}", Dot::new(&graph));
 }
 
 fn crawl_from_title(
@@ -67,10 +159,18 @@ fn crawl_from_title(
     }
     let mut links = get_wiki_links(wiki, title);
     if links.is_empty() {
-        println!("Received no links from page '{}'", title);
+        debug!(
+            target: DEBUG_TARGET,
+            "Received no links from page '{}'", title
+        );
         return;
     }
-    println!("Received {} links for page '{}'", links.len(), title);
+    debug!(
+        target: DEBUG_TARGET,
+        "Received {} links for page '{}'",
+        links.len(),
+        title
+    );
     links.shuffle(&mut rand::thread_rng());
 
     updates_so_far.extend(
@@ -88,34 +188,22 @@ fn crawl_from_title(
 
 fn update_graph(
     res: &Vec<(String, String)>,
-    graph: &mut petgraph::graph::Graph<String, String>,
+    graph: &mut petgraph::graph::UnGraph<String, String>,
 ) -> (i32, i32) {
     let mut tup = (0, 0);
     let tup_ref = &mut tup;
     res.iter().for_each(|adj| {
-        println!("IS IT HERE?");
         // let mut src_ni;
-        let src_ni = if let Some(node_idx) = graph.node_indices().find(|ni| {
-            if graph[*ni] == adj.0 {
-                println!("found {}", adj.0);
-                true
-            } else {
-                false
-            }
-        }) {
-            println!("Returning existing node {:?}", node_idx);
+        let src_ni = if let Some(node_idx) = graph.node_indices().find(|ni| graph[*ni] == adj.0) {
             node_idx
         } else {
             tup_ref.0 += 1;
-            println!("Adding {}", adj.0.to_owned());
             graph.add_node(adj.0.to_owned())
         };
         let dst_ni = if let Some(node_idx) = graph.node_indices().find(|ni| graph[*ni] == adj.1) {
-            println!("Returning existing node {:?}", node_idx);
             node_idx
         } else {
             tup_ref.0 += 1;
-            println!("Adding {}", adj.1.to_owned());
             graph.add_node(adj.1.to_owned())
         };
         graph.add_edge(src_ni, dst_ni, "".to_owned());
@@ -144,11 +232,17 @@ fn search_wiki(
                     ))
                 }
             }
-            Err(err) => Err(WikiError::new(format!("{}", term))),
+            Err(err) => Err(WikiError::new(format!(
+                "Error searching for '{}': {}",
+                term, err
+            ))),
         })
         .inspect(|r| match r {
-            Ok(results) => println!("Lookup for '{}': {}", results.0, results.1),
-            Err(err) => println!("Lookup failed: {}", err),
+            Ok(results) => debug!(
+                target: DEBUG_TARGET,
+                "Lookup for '{}': {}", results.0, results.1
+            ),
+            Err(err) => error!("Lookup failed: {}", err),
         })
         .collect::<std::result::Result<Vec<(String, String)>, WikiError>>()
         .unwrap_or(vec![])
